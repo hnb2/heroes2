dc3b4241a11fa2a50c9ae53eef3ed95cfcc52b46
Pierre Guillemot <pierreguillemot@yahoo.fr>
2018-01-14 18:11:06 +0400

    Moar content

--- src/app/adventure/adventure.component.ts
+++ src/app/adventure/adventure.component.ts
@@ -22,9 +22,11 @@ export class AdventureComponent implements OnInit {
 
   public adventures:Adventure[] = [
     {level: 1, text: 'You encounter a wild rabbit', creature: new Creature('Wild Rabbit', 1, 1)},
-    {level: 2, text: 'You encounter a wilder rabbit', creature: new Creature('Wilder Rabbit', 2, 2)},
-    {level: 3, text: 'You find nothing'},
-    {level: 4, text: 'You encounter the Rabbit Prince !', creature: new Creature('Rabbit Prince', 5, 5, 1)},
+    {level: 2, text: 'You encounter an even wilder rabbit', creature: new Creature('Wilder Rabbit', 2, 2)},
+    {level: 3, text: 'You hear some noise coming from behind that bush'},
+    {level: 4, text: 'It\'s the Rabbit Prince !', creature: new Creature('Rabbit Prince', 5, 5, 1)},
+    {level: 5, text: 'Well, well, well... Isn\'t it the Rabbit King ?!', creature: new Creature('Rabbit King', 10, 10, 2)},
+    {level: 6, text: 'You see a bunch of rabbits crying over their deceased monarchs...'},
   ];
 
   public currentAdventure:Adventure;
