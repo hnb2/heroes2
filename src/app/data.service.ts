00ebb674085011206632652f75bed79b21c94048
Pierre Guillemot <pierreguillemot@yahoo.fr>
2018-01-15 12:29:16 +0400

    Some more levels

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -27,7 +27,9 @@ export class DataService {
     {level: 5, text: 'Well, well, well... Isn\'t it the Rabbit King ?!', creature: new Creature('Rabbit King', 10, 10, 2)},
     {level: 6, text: 'You see a bunch of rabbits crying over their deceased monarchs...'},
     {level: 7, text: 'You were about to leave when you found what you where looking for', item: new Item('Holy Carrot')},
-    {level: 8, text: 'Maybe a delicious carrot juice before leaving ?', item: new Potion('Carrot Juice', 5)},
+    {level: 8, text: 'Oh noes ! Another tiny rabbit', creature: new Creature('Tiny Rabbit', 1, 1)},
+    {level: 9, text: 'Maybe a delicious carrot juice before leaving ?', item: new Potion('Carrot Juice', 5)},
+    {level: 10, text: 'You take the way back home with the Holy Carrot, and you know things will never be the same...'}
   ];
 
   public getAdventures():Adventure[] {
